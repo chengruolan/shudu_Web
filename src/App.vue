af1d1f2822074fbbdee6e442d6e725282a4f113f
chengruolan <chengruolan@digitalgd.com.cn>
2020-09-24 18:19:04 +0800

    20200924

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Layout>
+      <Header class="header">Header</Header>
+      <Content class="content">Content</Content>
+      <Footer class="footer">Footer</Footer>
+    </Layout>
   </div>
 </template>
+<script>
+import { Layout } from 'ant-design-vue'
+export default {
+  components: {
+    Header: Layout.Header,
+    Footer: Layout.Footer,
+    Content: Layout.Content,
+    Layout,
+  },
+  data() {
+    return {}
+  },
+}
+</script>
 
-<style>
+<style lang="less" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  color: #fff;
+  height: 100vh;
+  .header {
+    height: 80px;
+    line-height: 80px;
+  }
+  .content {
+    color: #000;
+    height: 80vh;
+    line-height: 80vh;
+  }
+  .footer {
+    color: #000;
+    height: 10vh;
+    line-height: 10vh;
+  }
 }
 </style>
